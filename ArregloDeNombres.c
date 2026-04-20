fb628c0b8b36e4d492d8f071d3e169511d53fdb0
ismaelisa <isaismael04@gmail.com>
2026-04-20 19:56:35 -0300

    rama busca-nombre

--- ArregloDeNombres.c
+++ ArregloDeNombres.c
@@ -1,21 +1,37 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-void MostrarPersonas(char ArreglodeNombres[5]);
+void MostrarPersonas(char *ArreglodeNombres[5]);
+void BuscarNombre(char *ArreglodeNombres[5], int ID);
 int main(){
     char *ArregloDeNombres[5];
-    char *buff[100];
+    char buff[100];
     for(int i = 0;i<5;i++){
-        scanf("%s", &buff);
+        printf("Ingrese el nombre: \n");
+        gets(buff);
         int cantidadDeCaracteres = strlen(buff);
-        ArregloDeNombres[i] = (char *)malloc(sizeof(char) * cantidadDeCaracteres);
+        ArregloDeNombres[i] = (char *)malloc(sizeof(char) * (cantidadDeCaracteres+1));
         strcpy(ArregloDeNombres[i],buff);
     }
-    MostrarPersonas(&ArregloDeNombres);
+    MostrarPersonas(ArregloDeNombres);
+    BuscarNombre(ArregloDeNombres, 0);
+    for(int i=0; i<5;i++){
+        free(ArregloDeNombres[i]);
+    }
     return 0;
 }
-void MostrarPersonas(char ArreglodeNombres[]){
+void MostrarPersonas(char *ArreglodeNombres[5]){
     for(int i=0; i<5; i++){
-        printf("%s \n", ArregloDeNombres[i]);
+        printf("%s \n", ArreglodeNombres[i]);
     }
 }
+void BuscarNombre(char *ArreglodeNombres[5], int ID){
+    if(ID>=0 && ID<=4){
+        printf("%s", ArreglodeNombres[ID]);
+    }
+    else{
+        printf("No se encontro el valor buscado");
+    }
+
+}
+
